23b38c5d1b50ce94b5cab76a8c6fe7e4ae7f9b2c
Tong My Tham <tongmytham@MacBook-Air-cua-Tong.local>
2022-05-17 22:18:56 +0700

    1st up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    latitude: null,
+    errMessage: "",
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({
+          errMessage: err.message,
+        });
+      }
+    );
+  }
+
+  renderContent = () => {
+    if (this.state.latitude && !this.state.errMessage) {
+      return <SeasonDisplay lat={this.state.latitude} />;
+    } else if (!this.state.latitude && this.state.errMessage) {
+      return <div> Error: {this.state.errMessage}</div>;
+    } else {
+      return <Spinner message="Please accept location request"/>;
+    }
+  };
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
 }
-
-export default App;
---
+++ src/SeasonDisplay.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./SeasonDisplay.css";
+
+const seasonConfig = {
+  winter: {
+    text: "Burr, it is chilly",
+    iconName: "snowflake",
+  },
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun",
+  },
+};
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  //   const text =
+  //     season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  //   const icon = season === "winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
